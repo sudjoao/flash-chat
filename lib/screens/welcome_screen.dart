fa169391424242ffddf291e9e85c5923c8cde70c
Joao Guedes <isudjoao@gmail.com>
2021-06-01 10:52:43 -0300

    Add routes to the project

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class WelcomeScreen extends StatefulWidget {
+  static const String id = "wecolme_screen";
   @override
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
